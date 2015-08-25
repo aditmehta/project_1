b65ea79714067f173c71a538550144dad497d4f1
aditmehta <aditmehta10@gmail.com>
2015-08-25 13:00:33 +0530

    added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+  sass: {                              // Task 
+    dist: {                            // Target
+      files: {                         // Dictionary of files 
+        'assets/stylesheets/styles.css': 'assets/stylesheets/styles.scss',       
+      }
+    }
+  },
+  watch: {
+    css: {
+      files: ['*.scss'],
+      tasks: ['sass'],
+    },
+  }
+});
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default',['sass','watch']);
+};
